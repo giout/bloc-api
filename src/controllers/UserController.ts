0be93061801ee4c686b776262222fff0741427f5
giout <giovanniurdanetatulli@gmail.com>
2023-10-01 14:36:01 -0400

    Componente de base de datos implementado (MongoService)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import MongoService from "./MongoService";
 import UserDocument from "../types/users";
 
-class UserController extends MongoService<UserDocument> {
-    
-}
+class UserController extends MongoService<UserDocument> {}
+
+export default UserController
