1d3053b92e0c795ec6238f8d2b13437c5e5bf966
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-10-10 17:02:25 -0400

    componente de mongo actualizado

--- src/controllers/MongoService.ts
+++ src/controllers/MongoService.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { Document, Model } from 'mongoose'
 
 class MongoService<D extends Document> {
@@ -8,51 +8,52 @@ class MongoService<D extends Document> {
         this.model = model
     }
 
-    public getAllEntries = async (req: Request, res: Response): Promise<void> =>{
-        this.handleHttp(req, res, async () => {
+    public getAllEntries = async (req: Request, res: Response, next:  NextFunction): Promise<void> =>{
+        try {
             const entries = await this.model.find()
-            res.status(200).json(entries)
-        })    
+            res.status(200).json(entries) 
+        } catch (err) {
+            next(err)
+        }
     }       
 
-    public getEntryById = async (req: Request, res: Response): Promise<void> =>{
-        this.handleHttp(req, res, async() => {
+    public getEntryById = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+        try {   
             const entry = await this.model.findById(req.params.id)
             res.status(200).json(entry)
-        })
+        } catch (err) {
+            next(err)
+        }
     }    
 
-    public createEntry = async (req: Request, res: Response): Promise<void> =>{
-        this.handleHttp(req, res, async() => {
+    public createEntry = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+        try {
             const entry = new this.model(req.body)
             await entry.save()
             res.status(201).end()
-        })
+        } catch (err) {
+            next(err)
+        }
     }    
 
-    public updateEntry = async (req: Request, res: Response): Promise<void> =>{
-        this.handleHttp(req, res, async() => {
+    public updateEntry = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+        try {
             const { params, body } = req
             await this.model.findByIdAndUpdate(params.id, body)
             res.status(200).end()
-        })
+        } catch (err) {
+            next(err)
+        }
     }    
 
-    public deleteEntry = async (req: Request, res: Response): Promise<void> =>{
-        this.handleHttp(req, res, async() => {
+    public deleteEntry = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+        try {
             await this.model.findByIdAndDelete(req.params.id)
             res.status(200).end()
-        })
-    }    
-
-    // handles try/catch, taking the try or success code block as parameter
-    protected handleHttp = async (req: Request, res: Response, operation: () => Promise<void>): Promise<void> =>{
-        try{
-            await operation()
-        } catch(err) {
-            res.status(500).json({msg: err})
+        } catch (err) {
+            next(err)
         }
-    }
+    }    
 }
 
 export default MongoService
